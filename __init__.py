8858c595e0a07c84178778437804f2dc0bdf2cb8
Michel Kohanim <michel@universal-devices.com>
2026-01-14 13:30:02 -0800

    initial release

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""NuCore Hebcal Holiday Provider Plugin"""
+from .nucore_holiday_provider  import NuCoreHolidayProvider, HolidayEvent    
+
+__version__ = "1.0.0"
+__all__ = ["NuCoreHolidayProvider", "HolidayEvent", ]
